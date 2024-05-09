6a336049ef75d4cbf0a5a0151314a8851c957eac
lyubomirageor <lyubomirageorgievaa@gmail.com>
2024-05-09 10:44:18 +0300

    Layer list button

--- map.js
+++ map.js
@@ -1,5 +1,5 @@
-require(["esri/config", "esri/WebMap", "esri/views/MapView", "esri/widgets/Home"],
-    function (esriConfig, WebMap, MapView, Home) {
+require(["esri/config", "esri/WebMap", "esri/views/MapView", "esri/widgets/Home", "esri/widgets/LayerList"],
+    function (esriConfig, WebMap, MapView, Home, LayerList) {
 
         esriConfig.apiKey = "AAPKfd648ea562964d168e3c24aa60b09114rjgbhvYT_hHEyS4z8w28nr2qpHAhQJOvUlgnWq0OT2MfajtPyBuiG0k0Hslv4x5a";
 
@@ -18,7 +18,19 @@ require(["esri/config", "esri/WebMap", "esri/views/MapView", "esri/widgets/Home"
             view 
         });
 
-        view.ui.add(homeBtn, "bottom-left");
+        view.ui.add(homeBtn, "top-left");
 
+        const layerList = new LayerList({
+            view
+        })
 
+        view.ui.add("layer-list-btn", "top-right");
+        view.ui.add(layerList, "top-right");
+        
+        document.getElementById("layer-list-btn").addEventListener("click", function(){
+            const layerListEl = document.getElementsByClassName("esri-layer-list")[0];
+            const currentProp = layerListEl.style.getPropertyValue("display");
+
+            layerListEl.style.setProperty("display", currentProp == "none"? "block" : "none");
+        })
     })
