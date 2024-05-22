63440b856d6e1b6a4d0ad4e38ecc64e3045666d5
lyubomirageor <lyubomirageorgievaa@gmail.com>
2024-05-22 17:44:14 +0300

    add directions

--- map.js
+++ map.js
@@ -1,18 +1,29 @@
-require(["esri/config", "esri/WebMap", "esri/views/MapView", "esri/widgets/Home", "esri/widgets/LayerList", "esri/widgets/BasemapGallery"],
-    function (esriConfig, WebMap, MapView, Home, LayerList, BasemapGallery) {
+require(["esri/config", "esri/WebMap", "esri/views/MapView", "esri/widgets/Home", "esri/widgets/LayerList", "esri/widgets/BasemapGallery", "esri/widgets/Directions", "esri/layers/RouteLayer"],
+    function (esriConfig, WebMap, MapView, Home, LayerList, BasemapGallery, Directions, RouteLayer) {
 
         esriConfig.apiKey = "AAPKfd648ea562964d168e3c24aa60b09114rjgbhvYT_hHEyS4z8w28nr2qpHAhQJOvUlgnWq0OT2MfajtPyBuiG0k0Hslv4x5a";
+
+        const routeLayer = new RouteLayer();
 
         const webmap = new WebMap({
             portalItem: {
                 id: "232b4d297d054b2a831a3ce629ac8495"
             }
         });
+        webmap.layers.add(routeLayer);
 
         const view = new MapView({
             container: "viewDiv",
             map: webmap
         });
+
+        const directionswidget = new Directions({
+            layer: routeLayer,
+            apiKey: esriConfig.apiKey,
+            view
+        });
+        
+        view.ui.add(directionswidget, {position: "bottom-left"});
 
         const homeBtn = new Home({
             view 
